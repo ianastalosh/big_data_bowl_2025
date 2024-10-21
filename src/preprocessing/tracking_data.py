2c267fc7491743fd72fb045bbb00d102d40908d8
ianastalosh <45411578+ianastalosh@users.noreply.github.com>
2024-10-21 13:30:38 +0000

    begin tracking processing

--- src/preprocessing/tracking_data.py
+++ src/preprocessing/tracking_data.py
@@ -1,6 +1,8 @@
 # Import and process tracking data
 import polars as pl
 import logging
+from config import Constants
+
 
 class TrackingDataProcessor:
     def __init__(self, tracking_data_file_paths: list[str]) -> None:
@@ -9,7 +11,7 @@ class TrackingDataProcessor:
         self.tracking_data_file_paths = tracking_data_file_paths
         self.tracking_data = None  # Initialize to None
 
-    def load_tracking_data(self) -> None:
+    def _load_tracking_data(self) -> None:
         tracking_data = pl.DataFrame()
         for file_path in self.tracking_data_file_paths:
             tracking_data = tracking_data.vstack(pl.scan_csv(file_path, ignore_errors=True))
@@ -17,8 +19,21 @@ class TrackingDataProcessor:
         self.tracking_data = tracking_data
         logging.info(f"Total dataframe has shape: {tracking_data.shape}")
     
-    def process_tracking_data(self) -> pl.DataFrame:
+    def _normalise_features(self) -> pl.DataFrame:
+        tracking_data = self.tracking_data
+
+        # Normalise features
+        tracking_data = tracking_data.with_column(
+            pl.when(pl.col("playDirection") == "left").then(pl.col("x").apply(lambda x: Constants.X_MAX - x)).otherwise(pl.col("x")).alias("x"),
+            pl.when(pl.col("playDirection") == "left").then(pl.col("y").apply(lambda y: Constants.Y_MAX - y)).otherwise(pl.col("y")).alias("y"),
+            # TODO: Check the logic of the dir normalisation, is different in R code
+            pl.when(pl.col("playDirection") == "left").then((pl.col("dir") + 180) % 360).otherwise(pl.col("dir")).alias("dir"),
+            pl.when(pl.col("playDirection") == "left").then((pl.col("o") + 180) % 360).otherwise(pl.col("o")).alias("o"),
+            
+        )
         pass
 
     def process(self):
-        pass
+        self._load_tracking_data()
+        self._normalise_features()
+        return self.tracking_data
