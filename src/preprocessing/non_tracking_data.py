0ec05f698f11d57649c31da3352d63574e8adb27
ianastalosh <45411578+ianastalosh@users.noreply.github.com>
2024-10-23 13:08:33 +0000

    progress on feature engineering

--- src/preprocessing/non_tracking_data.py
+++ src/preprocessing/non_tracking_data.py
@@ -1,27 +1,50 @@
 import polars as pl
 
 class NonTrackingDataProcessor:
-    def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
+    def __init__(self, 
+                 games_file_path: str,
+                 plays_file_path: str,
+                 player_file_path: str,
+                 player_plays_file_path: str) -> None:
+        self.games = self.load_data(games_file_path)
+        self.plays = self.load_data(plays_file_path)
+        self.players = self.load_data(player_file_path)
+        self.player_plays = self.load_data(player_plays_file_path)
 
     def load_data(self) -> pl.DataFrame:
         data = pl.read_csv(self.file_path)
         self.data = data
         return data
     
+    
+    def _join_games_plays_df(self) -> pl.DataFrame:
+        return self.plays.join(self.games, on="gameId", how="left")
+    
 
-class GamesProcessor(NonTrackingDataProcessor):
-    def preprocess(self) -> pl.DataFrame:
+    def _aggregate_player_plays_df(self) -> pl.DataFrame:
         pass
 
-class PlayerPlayProcessor(NonTrackingDataProcessor):
-    def preprocess(self) -> pl.DataFrame:
-        pass
+    def _feature_engineer(self) -> pl.DataFrame:
+        games_plays_joined = self._join_games_plays_df()
 
-class PlayersProcessor(NonTrackingDataProcessor):
-    def preprocess(self) -> pl.DataFrame:
-        pass
+        # Create possession team features
+        games_plays_joined = games_plays_joined.with_column(
+            # Get pre snap scores
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapHomeScore")).otherwise(pl.col("preSnapVisitorScore")).alias("preSnapPossessionTeamScore"),
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapVisitorScore")).otherwise(pl.col("preSnapHomeScore")).alias("preSnapDefensiveTeamScore"),
+            # Get pre snap win probability
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapHomeTeamWinProbability")).otherwise(pl.col("preSnapVisitorTeamWinProbability")).alias("preSnapPossessionTeamWP"),
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapVisitorTeamWinProbability")).otherwise(pl.col("preSnapHomeTeamWinProbability")).alias("preSnapDefensiveTeamWP"),
+            # Get win probability added
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("homeTeamWinProbabilityAdded")).otherwise(pl.col("visitorTeamWinProbilityAdded")).alias("possesionTeamWPAdded"),
+            pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("visitorTeamWinProbabilityAdded")).otherwise(pl.col("homeTeamWinProbilityAdded")).alias("defensiveTeamWPAdded"),
+            # Add defensive team expected points added
+            -1 * pl.col("expectedPointsAdded").alias("defensiveTeamExpectedPointsAdded"),
+            # Add score difference
+            pl.col("preSnapPossessionTeamScore") - pl.col("preSnapDefensiveTeamScore").alias("scoreDifference"),
+        )
 
-class PlaysProcessor(NonTrackingDataProcessor):
-    def preprocess(self) -> pl.DataFrame:
+        return games_plays_joined
+
+    def process(self):
         pass
