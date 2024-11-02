b7b023ba587e7bc7f3050fb6cefc026588cc74e1
ianastalosh <45411578+ianastalosh@users.noreply.github.com>
2024-11-02 13:36:36 +0000

    Add player play aggregation

--- src/preprocessing/non_tracking_data.py
+++ src/preprocessing/non_tracking_data.py
@@ -22,13 +22,58 @@ class NonTrackingDataProcessor:
     
 
     def _aggregate_player_plays_df(self) -> pl.DataFrame:
-        pass
+        """
+        Aggregate the player_plays df to get information on the number 
+        of players in motion/shifts at snap and lineset per play
 
-    def _feature_engineer(self) -> pl.DataFrame:
+        """
+        team_level_player_play_summary = self.player_plays.\
+            group_by(["gameId", "playId", "teamAbbr"]).\
+            agg((pl.col("inMotionAtBallSnap") == "TRUE").sum().alias("playNumPlayersInMotionAtSnap"),
+                (pl.col("shiftSinceLineset") == "TRUE").sum().alias("playNumPlayersShiftSinceLineset"),
+                (pl.col("motionSinceLineset") == "TRUE").sum().alias("playNumPlayersMotionSinceLineset"),
+                pl.col("yardageGainedAfterTheCatch").sum().alias("playYardageGainedAfterTheCatch"),
+                (pl.col("wasRunningRoute") == "1").sum().alias("playNumRoutesRun"),
+                ((pl.col("motionSinceLineset") == "TRUE") & (pl.col("wasTargettedReceiver") == 1)).sum().alias("playerMotionWasTargetted"),
+                ((pl.col("inMotionAtBallSnap") == "TRUE") & (pl.col("wasTargettedReceiver") == 1)).sum().alias("playerInMotionAtSnapWasTargetted")).\
+            with_columns(
+                (pl.col("playNumPlayersInMotionAtSnap") > 0).alias("playHadPlayersInMotionAtSnap"),
+                (pl.col("playNumPlayersMotionSinceLineset") > 0).alias("playHadPlayersMotionSinceLineset"),
+                (pl.col("playNumPlayersShiftSinceLineset") > 0).alias("playHadPlayersShiftSinceLineset")
+        )
+
+        return team_level_player_play_summary
+    
+    def _create_possession_team_player_play_features(self, aggregated_player_play_df) -> pl.DataFrame:
+        possession_team_df = aggregated_player_play_df.select(
+            gameId=pl.col("gameId"), 
+            playId=pl.col("playId"), 
+            possessionTeam = pl.col("teamAbbr"),
+            possessionTeamNumPlayersInMotionAtSnap = pl.col("playNumPlayersInMotionAtSnap"),
+            possessionTeamNumPlayersShiftSinceLineset = pl.col("playNumPlayersShiftSinceLineset"),
+            possessionTeamNumPlayersMotionSinceLineset = pl.col("playNumPlayersMotionSinceLineset"),
+            playerMotionWasTargetted = pl.col("playerMotionWasTargetted"),
+            playerInMotionAtSnapWasTargetted = pl.col("playerInMotionAtSnapWasTargetted"),
+        )
+
+        defensive_team_df = aggregated_player_play_df.select(
+            gameId=pl.col("gameId"), 
+            playId=pl.col("playId"), 
+            defensiveTeam = pl.col("teamAbbr"),
+            defensiveTeamNumPlayersInMotionAtSnap = pl.col("playNumPlayersInMotionAtSnap"),
+            defensiveTeamNumPlayersShiftSinceLineset = pl.col("playNumPlayersShiftSinceLineset"),
+            defensiveTeamNumPlayersMotionSinceLineset = pl.col("playNumPlayersMotionSinceLineset")
+        )
+
+        return possession_team_df, defensive_team_df
+    
+
+    def _feature_engineer_play_data(self) -> pl.DataFrame:
         games_plays_joined = self._join_games_plays_df()
+        aggregated_player_plays = self._aggregate_player_plays_df()
 
         # Create possession team features
-        games_plays_joined = games_plays_joined.with_column(
+        output_data = games_plays_joined.with_column(
             # Get pre snap scores
             pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapHomeScore")).otherwise(pl.col("preSnapVisitorScore")).alias("preSnapPossessionTeamScore"),
             pl.when(pl.col("possessionTeam") == pl.col("homeTeamAbbr")).then(pl.col("preSnapVisitorScore")).otherwise(pl.col("preSnapHomeScore")).alias("preSnapDefensiveTeamScore"),
@@ -44,7 +89,14 @@ class NonTrackingDataProcessor:
             pl.col("preSnapPossessionTeamScore") - pl.col("preSnapDefensiveTeamScore").alias("scoreDifference"),
         )
 
-        return games_plays_joined
+        # Join the aggregated player plays data
+        possession_player_plays, defensive_player_plays = self._create_possession_team_player_play_features(aggregated_player_plays)
+
+        output_data = output_data.\
+            join(possession_player_plays, on=["gameId", "playId", "possessionTeam"], how="left").\
+            join(defensive_player_plays, on=["gameId", "playId", "defensiveTeam"], how="left")
+
+        return output_data
 
     def process(self):
         pass
