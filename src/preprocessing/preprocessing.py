a02ca258c2886bea2cfa47659495acd8910104e7
ianastalosh <45411578+ianastalosh@users.noreply.github.com>
2024-12-24 03:14:11 +0000

    Add method to retrieve data for same play id

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -22,3 +22,17 @@ class BigDataBowlData:
         self.raw_player_plays = self.non_tracking_data_processor.player_plays
         self.tracking_data = self.tracking_data_processor.process()
         self.plays_df = self.non_tracking_data_processor.feature_engineer_play_data()
+
+    def _filter_play_id(self, lazy_frame: pl.LazyFrame, game_id: int, play_id: int) -> pl.LazyFrame:
+        return lazy_frame.filter(pl.col("gameId") == game_id).filter(pl.col("playId") == play_id)
+    
+    def get_tracking_data_for_play(self, game_id: int, play_id: int) -> dict:
+        play_tracking_data = self._filter_play_id(self.tracking_data, game_id, play_id).collect()
+        play_plays_df = self._filter_play_id(self.plays_df, game_id, play_id).collect()
+        play_player_plays = self._filter_play_id(self.raw_player_plays, game_id, play_id).collect()
+        
+        return {
+            "tracking_data": play_tracking_data,
+            "plays_df": play_plays_df,
+            "player_plays": play_player_plays
+        }
